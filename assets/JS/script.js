f0549ae9605abe5192322bc8d38742ec4b8f3aff
Douglas-Dauto <dautodouglasdauto@gmail.com>
2023-07-22 10:36:53 -0300

    Corrigido problema nas curtidas

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,10 +2,11 @@
 const img = window.document.querySelectorAll('.main_content10-img-desenho>img');
 const svg = window.document.getElementsByClassName('svg-img');
 const path = window.document.getElementsByClassName('path-img');
-let indiceArmz, curtirValue = true;
 
 curtir();
 function curtir() {
+    let indiceArmz, curtirValue = true;
+
     for(let indice = 0; indice < img.length; indice++) {
         img[indice].addEventListener('dblclick', () => curtirChange(indice));
         svg[indice].addEventListener('click', () => curtirChange(indice));
@@ -16,34 +17,34 @@ function curtir() {
             path[indice].setAttribute("class", "path-img path-img--red");
             indiceArmz = indice;
             curtirValue = false;
-        } else if(curtirValue === false && indiceArmz === indice) {
+        } else if(curtirValue === false || indiceArmz === indice) {
             path[indice].setAttribute("class", "path-img path-img--black");
             curtirValue = true;
         }
     }
 }
 
-abrirFecharMenuMobile(true);
-function abrirFecharMenuMobile(valor) {
-    if(valor) {
-        buttonMenuMobile.addEventListener('click', () => {
-            menuMobile.setAttribute('id', 'div-logo-lista-conjunto-087--return');
-            abrirFecharMenuMobile(false);
-        });
-    } else {
-        buttonMenuMobile.addEventListener('click', removerMenu);
-        menuMobile.addEventListener('click', removerMenu);
-        document.body.style.overflowY = 'hidden';
+// abrirFecharMenuMobile(true);
+// function abrirFecharMenuMobile(valor) {
+//     if(valor) {
+//         buttonMenuMobile.addEventListener('click', () => {
+//             menuMobile.setAttribute('id', 'div-logo-lista-conjunto-087--return');
+//             abrirFecharMenuMobile(false);
+//         });
+//     } else {
+//         buttonMenuMobile.addEventListener('click', removerMenu);
+//         menuMobile.addEventListener('click', removerMenu);
+//         document.body.style.overflowY = 'hidden';
 
-        for(let i = 0; i < divPartMenuMobile.length; i++) {
-            divPartMenuMobile[i].addEventListener('click', removerMenu);
-        }
-    }
-}
+//         for(let i = 0; i < divPartMenuMobile.length; i++) {
+//             divPartMenuMobile[i].addEventListener('click', removerMenu);
+//         }
+//     }
+// }
 
-function removerMenu() {
-    menuMobile.removeAttribute('id', 'div-logo-lista-conjunto-087--return');
-    abrirFecharMenuMobile(true);
-    document.body.style.overflowY = 'scroll';
-}
+// function removerMenu() {
+//     menuMobile.removeAttribute('id', 'div-logo-lista-conjunto-087--return');
+//     abrirFecharMenuMobile(true);
+//     document.body.style.overflowY = 'scroll';
+// }
 })();
